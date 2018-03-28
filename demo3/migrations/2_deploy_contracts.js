df74441c0b54ac4a8e3bafbbe593e3c79fd6bfc3
Brian Ho <mr.brianhobo@hotmail.com>
2018-03-28 13:51:41 -0700

    changed file names

--- demo3/migrations/2_deploy_contracts.js
+++ demo3/migrations/2_deploy_contracts.js
@@ -4,6 +4,6 @@ var SimpleBank = artifacts.require("./SimpleBank.sol");
 module.exports = function(deployer) {
 	/* Input value to constructor on contract deployment */
 	var testGreeting = "Hello, World!";
-    deployer.deploy(Greeter, testGreeting);
-    deployer.deploy(SimpleBank);
+	deployer.deploy(Greeter, testGreeting);
+	deployer.deploy(SimpleBank);
 };
